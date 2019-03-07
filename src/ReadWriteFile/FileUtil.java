50881311d11c9bdc9aa4a19cbf5eaa36b05f9ed1
wulfoski <Iosif*farcas88>
2019-03-07 21:59:29 +0200

    first commit

---
+++ src/ReadWriteFile/FileUtil.java
@@ -0,0 +1,24 @@
+package ReadWriteFile;
+
+import java.io.*;
+
+public class FileUtil {
+
+    public void readWriteFile(String inFilePath, String outFilePath) {
+        String readLine = null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inFilePath));
+             Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFilePath)))
+        ) {
+            while ((readLine = bufferedReader.readLine()) != null) {
+
+                writer.write(readLine);
+                writer.write(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            System.out.println("You got an error!");
+            e.printStackTrace();
+        }
+    }
+}
+
+
---
+++ src/ReadWriteFile/Main.java
@@ -0,0 +1,9 @@
+package ReadWriteFile;
+
+public class Main {
+    public static void main(String[] args) {
+        FileUtil fileUtil = new FileUtil();
+        fileUtil.readWriteFile("src/InputFile.txt", "src/OutputFile.txt");
+
+    }
+}
